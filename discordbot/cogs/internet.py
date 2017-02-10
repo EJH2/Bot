f504fe76f9265520c96f4ac6a51f1064ec589b37
Ethan <ethan.harriz@gmail.com>
2017-02-09 20:01:18 -0500

    updated internet for rewrite

--- discordbot/cogs/internet.py
+++ discordbot/cogs/internet.py
@@ -22,56 +22,56 @@ class Internet:
         self.bot = bot
 
     @commands.command()
-    async def penguin(self):
+    async def penguin(self, ctx):
         """
         Penguins!
         """
         url = "http://penguin.wtf/"
         url1 = await util.get_file(url)
         file = await util.get_file(url1.decode())
-        await self.bot.upload(io.BytesIO(file), filename="penguin.png")
+        await ctx.send(file=io.BytesIO(file), filename="penguin.png")
 
     @commands.command()
-    async def rip(self, user: discord.User):
+    async def rip(self, ctx, user: discord.User = None):
         """
         RIP.
         """
         user = user.display_name
-        await self.bot.say("<http://ripme.xyz/{}>".format(user.replace(" ", "%20")))
+        await ctx.send("<http://ripme.xyz/{}>".format(user.replace(" ", "%20")))
 
     @commands.command()
-    async def robohash(self, user: discord.User):
+    async def robohash(self, ctx, user: discord.User):
         """
         Robot pics.
         """
         user = user.display_name
-        await self.bot.say("https://robohash.org/{}.png".format(user.replace(" ", "%20")))
+        await ctx.send("https://robohash.org/{}.png".format(user.replace(" ", "%20")))
 
     @commands.command()
-    async def xkcd(self, query=""):
+    async def xkcd(self, ctx, query=""):
         """Queries a random XKCD comic.
 
         Do `^xkcd <number from 1-1662>` to pick a specific comic."""
         if not query:
             i = randint(1, 1662)
             url = "https://xkcd.com/{}/".format(i)
-        elif query.isdigit() and 1 <= int(query) <= 1662:
+        elif query.isdigit() and 1 <= int(query) <= 1796:
             url = "https://xkcd.com/{}/".format(query)
-        elif int(query) <= 0 or int(query) >= 1663:
-            await self.bot.say("It has to be between 1 and 1662!")
+        elif int(query) <= 0 or int(query) >= 1796:
+            await ctx.send("It has to be between 1 and 1796!")
         elif not query.isdigit():
-            await self.bot.say("You have to put a number!")
+            await ctx.send("You have to put a number!")
         else:
-            await self.bot.say("I don't know how you managed to do it, but you borked it.")
+            await ctx.send("I don't know how you managed to do it, but you borked it.")
         with aiohttp.ClientSession() as session:
             async with session.get(url) as resp:
                 r = await resp.read()
         resp = BSoup(r, "html.parser")
-        await self.bot.say(
+        await ctx.send(
             ":mag:**" + resp("img")[1]["alt"] + "**\nhttp:" + resp("img")[1]["src"] + "\n" + resp("img")[1]["title"])
 
     @commands.command()
-    async def weather(self, *, location):
+    async def weather(self, ctx, *, location):
         """
         Gives the current weather in a city.
         """
@@ -80,11 +80,11 @@ class Internet:
         w = observation.get_weather()
         obs = w.get_detailed_status()
         if obs == "clear sky":
-            await self.bot.say("The weather is forecast to be a clear sky :sunny:")
+            await ctx.send("The weather is forecast to be a clear sky :sunny:")
         elif obs == "broken clouds":
-            await self.bot.say("The weather is forecast to be broken clouds :cloud:")
+            await ctx.send("The weather is forecast to be broken clouds :cloud:")
         else:
-            await self.bot.say("The weather is forecast to be " + "".join(map(str, obs)) + ".")
+            await ctx.send("The weather is forecast to be " + "".join(map(str, obs)) + ".")
 
 
 def setup(bot: DiscordBot):
