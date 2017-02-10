e6430951714e9aee98fdf7727bd018c27b90b45d
Ethan <ethan.harriz@gmail.com>
2017-02-09 20:01:52 -0500

    updated fun for rewrite

--- discordbot/cogs/fun.py
+++ discordbot/cogs/fun.py
@@ -21,121 +21,125 @@ class Fun:
     def __init__(self, bot: DiscordBot):
         self.bot = bot
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def shoot(self, ctx, *members: discord.Member):
         """
         Allows the user to shoot a person of choice.
         """
         if not members:
-            await self.bot.say("You gotta give me someone to work with here!")
+            await ctx.send("You gotta give me someone to work with here!")
             return
         for member in members:
             if member.id == self.bot.user.id:
-                await self.bot.say(
+                await ctx.send(
                     "You attempted to shoot me, {}, but I dodged it!\n"
                     "http://45.media.tumblr.com/c1165e983042a9cd1f17028a1c78170b/tumblr_n9c38m14291s5f9ado1_500.gif"
                     .format(ctx.message.author.name))
             elif member.id == ctx.message.author.id:
-                await self.bot.say(
+                await ctx.send(
                     "{} committed suicide!\nhttps://media.giphy.com/media/5xaOcLAo1Gg0oRgBz0Y/giphy.gif".format(
                         ctx.message.author.name))
             else:
-                await self.bot.say(
+                await ctx.send(
                     "{1} was shot dead by the mighty {0}!\n"
                     "https://s-media-cache-ak0.pinimg.com/originals/2d/fa/a9/2dfaa995a09d81a07cad24d3ce18e011.gif"
                     .format(ctx.message.author.name, member.name))
 
     @commands.command()
-    async def rr(self):
+    async def rr(self, ctx):
         """Allows the user to take part in the famous Russian Pasttime."""
-        await self.bot.say('You spin the cylinder of the revolver with 1 bullet in it...')
+        await ctx.send('You spin the cylinder of the revolver with 1 bullet in it...')
         await asyncio.sleep(1)
-        await self.bot.say('...you place the muzzle against your head and pull the trigger...')
+        await ctx.send('...you place the muzzle against your head and pull the trigger...')
         await asyncio.sleep(2)
         global rr_bullet, rr_count
         if rr_bullet == rr_count:
-            await self.bot.say('...your brain gets splattered all over the wall.')
+            await ctx.send('...your brain gets splattered all over the wall.')
             rr_bullet = random.randint(1, 6)
             rr_count = 1
         else:
-            await self.bot.say('...you live to see another day.')
+            await ctx.send('...you live to see another day.')
             rr_count += 1
 
     @commands.command()
-    async def lmgtfy(self, *, query: str):
+    async def lmgtfy(self, ctx, *, query: str):
         """
         Gives the user a "Let Me Google That For You" link.
         """
         msg = query.replace(" ", "+")
         msg = "http://lmgtfy.com/?q={}".format(msg)
-        await self.bot.say(msg)
+        await ctx.send(msg)
 
     @commands.command()
-    async def meh(self):
+    async def meh(self, ctx):
         """
         Meh.
         """
-        await self.bot.say("¯\_(ツ)_/¯")
+        await ctx.send("¯\_(ツ)_/¯")
 
     @commands.command()
-    async def say(self, *, message: str):
+    async def say(self, ctx, *, message: str):
         """
         Makes the bot say anything the user wants it to.
         """
-        await self.bot.say(message)
+        await ctx.send(message)
 
-    @commands.command(pass_context=True)
+    @commands.command()
     async def timer(self, ctx, seconds: int, *, remember: str = ""):
         """
         Sets a timer for a user with the option of setting a reminder text.
         """
         if not remember:
-            end_timer = self.bot.say("{}, your timer for {} seconds has expired!".format(ctx.message.author.name,
+            end_timer = ctx.send("{}, your timer for {} seconds has expired!".format(ctx.message.author.name,
                                                                                          seconds))
-            await self.bot.say("{}, you have set a timer for {} seconds!".format(ctx.message.author.name, seconds))
-            timer = await self.bot.wait_for_message(author=ctx.message.author,
-                                                    content="{0.bot.command_prefix}cancel".format(self),
-                                                    timeout=seconds)
+            await ctx.send("{}, you have set a timer for {} seconds!".format(ctx.message.author.name, seconds))
+
+            def check(m):
+                return m.author == ctx.message.author and ctx.message.content == "{0.bot.command_prefix}cancel".format(
+                    ctx)
+            timer = await ctx.wait_for(check=check, timeout=seconds)
             if timer is None:
                 await end_timer
                 return
-            await self.bot.reply("Cancelling your timer...")
+            await ctx.send("{}, Cancelling your timer...".format(ctx.message.author.mention))
         else:
-            end_timer = self.bot.say("{}, your timer for {} seconds has expired! I was instructed to remind you about "
-                                     "`{}`!".format(ctx.message.author.mention, seconds, remember))
-            await self.bot.say("{}, I will remind you about `{}` in {} seconds!".format(ctx.message.author.mention,
-                                                                                        seconds, remember))
-            timer = await self.bot.wait_for_message(author=ctx.message.author,
-                                                    content="{0.bot.command_prefix}cancel".format(self),
-                                                    timeout=seconds)
+            end_timer = ctx.send("{}, your timer for {} seconds has expired! I was instructed to remind you about "
+                                 "`{}`!".format(ctx.message.author.mention, seconds, remember))
+            await ctx.send("{}, I will remind you about `{}` in {} seconds!".format(ctx.message.author.mention,
+                                                                                    seconds, remember))
+
+            def check(m):
+                return m.author == ctx.message.author and ctx.message.content == "{0.bot.command_prefix}cancel".format(
+                    ctx)
+            timer = await ctx.wait_for(check=check, timeout=seconds)
             if timer is None:
                 await end_timer
                 return
-            await self.bot.reply("Cancelling your timer...")
+            await ctx.send("{}, Cancelling your timer...".format(ctx.message.author.mention))
 
     @commands.command()
-    async def shame(self):
+    async def shame(self, ctx):
         """
         Let the shame bells ring!
         """
-        await self.bot.say(":bell::bell::bell: ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ "
-                           "​:bell: ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​:bell:"
-                           " ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​:bell::bell:"
-                           ":bell:\n:bell: ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​:bell:"
-                           " ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​:bell:"
-                           " ​ ​ ​ ​ ​ ​ ​:bell::bell::bell::bell: ​ ​ ​ ​ ​ ​:bell:\n:bell:"
-                           ":bell::bell: ​ ​ ​ ​ ​ ​ ​:bell::bell::bell:"
-                           " ​ ​ ​ ​ ​ ​ ​:bell::bell::bell: ​ ​ ​ ​ ​ ​ ​:bell: ​ ​:bell:"
-                           " ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​:bell::bell::bell:\n"
-                           "              :bell: ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​:bell:"
-                           " ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ "
-                           "​ ​ ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​:bell:\n:bell::bell:"
-                           ":bell: ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​:bell:"
-                           " ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​:bell:"
-                           " ​ ​ ​ ​ ​ ​ ​:bell::bell:​:bell:\n")
+        await ctx.send(":bell::bell::bell: ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ "
+                       "​:bell: ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​:bell:"
+                       " ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​:bell::bell:"
+                       ":bell:\n:bell: ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​:bell:"
+                       " ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​:bell:"
+                       " ​ ​ ​ ​ ​ ​ ​:bell::bell::bell::bell: ​ ​ ​ ​ ​ ​:bell:\n:bell:"
+                       ":bell::bell: ​ ​ ​ ​ ​ ​ ​:bell::bell::bell:"
+                       " ​ ​ ​ ​ ​ ​ ​:bell::bell::bell: ​ ​ ​ ​ ​ ​ ​:bell: ​ ​:bell:"
+                       " ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​:bell::bell::bell:\n"
+                       "              :bell: ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​:bell:"
+                       " ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ "
+                       "​ ​ ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​:bell:\n:bell::bell:"
+                       ":bell: ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​:bell:"
+                       " ​ ​ ​ ​ ​ ​ ​:bell: ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​ ​:bell:"
+                       " ​ ​ ​ ​ ​ ​ ​:bell::bell:​:bell:\n")
 
     @commands.command()
-    async def fancify(self, *, text: str):
+    async def fancify(self, ctx, *, text: str):
         """
         Fancify text.
         """
@@ -147,28 +151,28 @@ class Fun:
                 output += chr(ord(letter) + 119919)
             elif letter == " ":
                 output += " "
-        await self.bot.say(output)
+        await ctx.send(output)
 
     @commands.command()
-    async def bigtext(self, *, text):
+    async def bigtext(self, ctx, *, text):
         """
         Create enlarged text.
         """
-        await self.bot.say("```fix\n" + figlet_format(text, font="big") + "```")
+        await ctx.send("```fix\n" + figlet_format(text, font="big") + "```")
 
     @commands.command()
     @commands.cooldown(1, 20, BucketType.channel)
-    async def repeat(self, times: int, *, content: str):
+    async def repeat(self, ctx, times: int, *, content: str):
         """
         Repeats a message x times.
         """
         if times > 50:
-            await self.bot.say("I can't repeat that that many times! I might choke!")
+            await ctx.send("I can't repeat that that many times! I might choke!")
             return
         for x in range(0, times):
-            await self.bot.say(content)
+            await ctx.send(content)
             await asyncio.sleep(1)
-        await self.bot.say("Done!")
+        await ctx.send("Done!")
 
 
 def setup(bot: DiscordBot):
