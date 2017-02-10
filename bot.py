e92b98d77a487a335abde030e57c50c9e1c973bd
Ethan <ethan.harriz@gmail.com>
2017-02-09 19:59:07 -0500

    Updated bot.py for rewrite

--- bot.py
+++ bot.py
@@ -2,9 +2,12 @@
 """
 Bot runner.
 """
+import os
+
 from discordbot.bot import DiscordBot
 from discordbot.consts import bot_config
 
 if __name__ == "__main__":
     bot = DiscordBot(command_prefix=bot_config["bot"]["command_prefix"], description="")
+    bot.filename = os.path.basename(__file__)
     bot.run()
