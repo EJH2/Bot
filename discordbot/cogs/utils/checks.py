27da804b22a429f9db1b5a49b0b1cd8fb1c416dc
Ethan <ethan.harriz@gmail.com>
2017-02-09 20:02:46 -0500

    updated checks for rewrite

--- discordbot/cogs/utils/checks.py
+++ discordbot/cogs/utils/checks.py
@@ -15,7 +15,7 @@ def is_owner(ctx):
 def is_server_owner(ctx):
     if is_owner:
         return True
-    return ctx.message.author.id == ctx.message.server.owner.id
+    return ctx.message.author.id == ctx.message.guild.owner.id
 
 
 def check_permissions(ctx, perms):
@@ -35,10 +35,16 @@ def role(role_name):
     def predicate(ctx):
         if is_server_owner:
             return True
-        if ctx.message.server:
-            for role in ctx.message.server.me.roles:
+        if ctx.message.guild:
+            for role in ctx.message.guild.me.roles:
                 if role.name.lower() == role_name.lower():
                     return True
         raise exceptions.ClearanceError("Bot requires role \"{}\" to run that command.".format(role_name.title()))
 
     return commands.check(predicate)
+
+
+def needs_embed(ctx):
+    if ctx.message.channel.permissions_for(ctx.message.guild.me).embed_links:
+        return True
+    raise exceptions.EmbedError
