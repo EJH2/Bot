79ac29b1765c1f15dab8fbd76469ff96d89e2b3c
Ethan <ethan.harriz@gmail.com>
2017-08-21 13:18:23 -0400

    Actually restart the whole bot this time.

--- bot.py
+++ bot.py
@@ -2,47 +2,49 @@
 Bot runner.
 """
 import asyncio
+import importlib
 import os
 import sys
 import time
 
 from discord.ext import commands
 
-from discordbot.bot import DiscordBot
+from discordbot import bot
 from discordbot.consts import bot_config, init_modules
 
 kwargs = {"command_prefix": commands.when_mentioned_or(bot_config["bot"]["command_prefix"]), "description": "",
           "formatter": commands.HelpFormatter(show_check_failure=True)}
 
 
+# Initialising the bot instance, along with some helpful extras
 def init():
-    # Initialising the bot instance, along with some helpful extras
-    bot = DiscordBot(**kwargs)
-    bot.filename = os.path.basename(__file__)
-    bot.debug = any('debug' in arg.lower() for arg in sys.argv)
+    importlib.reload(bot)  # This will make sure we reload *all* of the bot, and not just the cogs
+    discord_bot = bot.DiscordBot(**kwargs)
+    discord_bot.filename = os.path.basename(__file__)
+    discord_bot.debug = any('debug' in arg.lower() for arg in sys.argv)
 
-    return bot
+    return discord_bot
 
 
 def restart():
-    bot = init()
-    return bot
+    discord_bot = init()
+    return discord_bot
 
 
 def main():
-    bot = restart()
-    if bot.debug:
+    discord_bot = restart()
+    if discord_bot.debug:
         init_modules.remove("discordbot.cogs.init_stats")
-    bot.run()
-    while bot.restarting.get('restarting', False):
+    discord_bot.run()
+    while discord_bot.restarting.get('restarting', False):
         print("Restarting in 5 seconds...", flush=True)
         time.sleep(5)
         print('Attempting to restart...\n'
               "-------------------"
               "\n")
         asyncio.set_event_loop(asyncio.new_event_loop())
-        bot = restart()
-        bot.run()
+        discord_bot = restart()
+        discord_bot.run()
     return input("Press any key to continue...")
 
 
