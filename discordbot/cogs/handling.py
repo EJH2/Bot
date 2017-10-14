c7c13973d77c68583c8b4c09d8c6042d6505f6cc
Ethan <ethan.harriz@gmail.com>
2017-10-14 15:43:42 -0400

    Heyy Sentry support!

--- discordbot/cogs/handling.py
+++ discordbot/cogs/handling.py
@@ -7,6 +7,7 @@ import traceback
 
 import discord
 from discord.ext import commands
+from raven import Client
 
 from discordbot.main import DiscordBot
 from discordbot.utils import exceptions
@@ -19,6 +20,7 @@ class CommandHandler:
 
     def __init__(self, bot: DiscordBot):
         self.bot = bot
+        self.sentry = self.get_sentry()
 
     async def __global_check(self, ctx):
 
@@ -44,56 +46,77 @@ class CommandHandler:
 
         return True
 
+    def get_sentry(self):
+        if self.bot.config["sentry"] is not None:
+            sentry = Client(self.bot.config["sentry"], enable_breadcrumbs=False)
+            return sentry
+
     async def on_message_edit(self, before, after):
         """
         Checks message edit to see if I screwed up a command...
         """
         await self.bot.process_commands(after)
 
-    @staticmethod
-    async def on_command_error(ctx, e):
+    async def on_error(self, event_method):
+        """
+        Catches non-command errors
+        """
+        print('Ignoring exception in {}'.format(event_method), file=sys.stderr)
+        traceback.print_exc()
+        self.sentry.captureException()
+        self.bot.logger.warn("Error sent to Sentry!")
+
+    async def on_command_error(self, ctx, error):
         """
         Catch command errors.
         """
-        if isinstance(e, exceptions.Ignored):
+        if isinstance(error, exceptions.Ignored):
             await ctx.channel.send("\N{CROSS MARK} This channel is currently being ignored.", delete_after=5)
-        elif isinstance(e, commands.errors.NotOwner):
-            await ctx.channel.send(f"\N{CROSS MARK} {e}", delete_after=5)
-        elif isinstance(e, discord.errors.Forbidden):
+        elif isinstance(error, commands.errors.NotOwner):
+            await ctx.channel.send(f"\N{CROSS MARK} {error}", delete_after=5)
+        elif isinstance(error, discord.errors.Forbidden):
             await ctx.channel.send("\N{NO ENTRY} I don't have permission to perform the action", delete_after=5)
-        elif isinstance(e, exceptions.ClearanceError):
-            await ctx.channel.send(f"\N{NO ENTRY} {e}", delete_after=5)
-        elif isinstance(e, commands.errors.CommandNotFound):
+        elif isinstance(error, exceptions.ClearanceError):
+            await ctx.channel.send(f"\N{NO ENTRY} {error}", delete_after=5)
+        elif isinstance(error, commands.errors.CommandNotFound):
             return
-        elif isinstance(e.__cause__, discord.errors.NotFound):
+        elif isinstance(error.__cause__, discord.errors.NotFound):
             return
-        elif isinstance(e, exceptions.EmbedError):
+        elif isinstance(error, exceptions.EmbedError):
             await ctx.channel.send("\N{NO ENTRY} This command requires the `Embed Links` "
                                    "permission to execute!", delete_after=5)
-        elif isinstance(e, commands.errors.NoPrivateMessage):
+        elif isinstance(error, commands.errors.NoPrivateMessage):
             await ctx.channel.send("\N{NO ENTRY} That command can not be run in PMs!",
                                    delete_after=5)
             return
-        elif isinstance(e, commands.errors.DisabledCommand):
+        elif isinstance(error, commands.errors.DisabledCommand):
             await ctx.channel.send("\N{NO ENTRY} Sorry, but that command is currently disabled!",
                                    delete_after=5)
-        elif isinstance(e, commands.errors.CheckFailure):
+        elif isinstance(error, commands.errors.CheckFailure):
             await ctx.channel.send("\N{CROSS MARK} Check failed. You probably don't have "
                                    "permission to do this.", delete_after=5)
-        elif isinstance(e, commands.errors.CommandOnCooldown):
-            await ctx.channel.send(f"\N{NO ENTRY} {e}", delete_after=5)
-        elif isinstance(e, (commands.errors.BadArgument, commands.errors.MissingRequiredArgument)):
-            await ctx.channel.send(f"\N{CROSS MARK} Bad argument: {' '.join(e.args)}", delete_after=5)
+        elif isinstance(error, commands.errors.CommandOnCooldown):
+            await ctx.channel.send(f"\N{NO ENTRY} {error}", delete_after=5)
+        elif isinstance(error, (commands.errors.BadArgument, commands.errors.MissingRequiredArgument)):
+            await ctx.channel.send(f"\N{CROSS MARK} Bad argument: {' '.join(error.args)}", delete_after=5)
             formatted_help = await ctx.bot.formatter.format_help_for(ctx, ctx.command)
             for page in formatted_help:
                 await ctx.channel.send(page, delete_after=20)
         else:
             await ctx.channel.send("\N{NO ENTRY} An error happened. This has been logged and reported.",
                                    delete_after=5)
-            if isinstance(e, commands.errors.CommandInvokeError):
-                traceback.print_exception(type(e), e.__cause__, e.__cause__.__traceback__, file=sys.stderr)
+            if isinstance(error, commands.errors.CommandInvokeError):
+                traceback.print_exception(type(error), error.__cause__, error.__cause__.__traceback__, file=sys.stderr)
             else:
-                traceback.print_exception(type(e), e, e.__traceback__, file=sys.stderr)
+                traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
+            if self.sentry:
+                try:
+                    raise error.original
+                except:
+                    assert isinstance(self.sentry, Client)
+                    self.sentry.captureException(data={'message': ctx.message.content}, extra={'ctx': ctx.__dict__,
+                                                                                               'error': error})
+            self.bot.logger.warn("Error sent to Sentry!")
 
     async def on_command(self, ctx):
         author = ctx.author
